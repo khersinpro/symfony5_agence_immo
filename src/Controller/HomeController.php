da4d8b860d89d489bfc1356a0f06d3d15ad93342
khersinpro <khersinpro@gmail.com>
2022-12-26 16:49:23 +0100

    setting PropertyEntity, HomeController, PropertyController (show all article or show specific article)"

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PropertyRepository $repository): Response
     {
+        $properties = $repository->findLatest();
         return $this->render('pages/home.html.twig', [
-            'controller_name' => 'HomeController',
+            'properties' => $properties,
         ]);
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,19 +2,61 @@
 
 namespace App\Controller;
 
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PropertyController extends AbstractController
 {
+
+    private $propertyRepository;
+
+    public function __construct(PropertyRepository $propertyRepository)
+    {
+        $this->propertyRepository = $propertyRepository;
+    }
     /**
      * @Route("/biens", name="property.index")
      */
     public function index(): Response
     {
+
+        // $property = new Property();
+        // $property->setTitle('Mon premier bien')
+        //     ->setDescription('Ce bien est vraiment trés jolie.')
+        //     ->setSurface(300)
+        //     ->setRooms(12)
+        //     ->setBedrooms(5)
+        //     ->setFloor(2)
+        //     ->setPrice(150000)
+        //     ->setHeat(1)
+        //     ->setCity('Marseille')
+        //     ->setAddress('ceci est une adress')
+        //     ->setPostalCode('code postal');
+        // $propertyRepository->add($property, true);
+
         return $this->render('property/index.html.twig', [
             'current_menu' => 'properties',
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show($slug, $id, Property $property): Response
+    {
+        if ($slug !== $property->getSlug()) {
+            return $this->redirectToRoute('property.show', [
+                'slug' => $property->getSlug(), 
+                'id' => $id
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties',
+            'property' => $property
         ]);
     }
 }
